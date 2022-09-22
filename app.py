e3a346f942cb6b406c7f968270ee9f9ab2f62788
jamesrcutmore <robcutmore36@gmail.com>
2022-09-22 19:05:28 +0000

    pthon,flask user sign in and logout

--- app.py
+++ app.py
@@ -8,8 +8,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-                        
-                        
+
+@app.route('/index.html')
+def home():
+    return render_template('index.html')
+
+@app.route('/recipes.html')
+def recipes():
+    return render_template('recipes.html')                        
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')                        
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')  
+
+@app.route('/recipes.json')
+def recipesJSON():
+    return render_template('recipes.json') 
+
+@app.route('/login', methods=['POST'])
+def dashboard():
+    return render_template('dashboard.html')                       
+
+
 
 if __name__ == '__main__':
     app.debug = True
