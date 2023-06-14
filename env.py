69280b178f3889597013a646f771e36031d30859
James cutmore <robcutmore36@gmail.com>
2023-06-14 21:33:27 +0000

    env update

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "james_rob_cut_more")
-os.environ.setdefault("MONGO_URI", "")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://Smoothie:Sm00thieUser@cluster0.kuaea3o.mongodb.net/?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "supersmoothie")
